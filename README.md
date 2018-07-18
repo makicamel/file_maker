f4e4a02c7bacaaca3c210b6ab7ca0196d58ad583
Maki Kawahara <maki.kawahara@onsuku.jp>
2018-07-18 10:25:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# What's FileMaker by Regexp ?
+  This is a tool for making new files by regular expression.
+  This tool reads a file written in regular expression and makes new files of picking phrases.
+  This'll help at the time you want to pick phrases from a big log file or many files.
+# Usage
+  1, Put a file named 'regexp.txt' written in regular expression and some files you want to trim same directory.
+  2, Run this script like; 'ruby trim_file.rb'
+  3, And input ** absolute ** path to the directory. ex) /path/to/directory
+  4, Then you can find new files at the specified folder.
+# Notice
+  - You can write some lines not only one line in the regexp.txt.
+  - If you want to change the regular expression file name or folder name for new files,
+    you can custom them by changing constant 'REGEXP_FILE' or 'NEW_FOLDER'.
+# Contributing
+  This is my first tool.
+  I'm veeeeery grad if you file an issue, pull request or something.
+# License
+  Copyright (c) 2018 Maki Kawahara
+  Released under the MIT license
+  https://opensource.org/licenses/mit-license.php
+
+# ファイルメイカー from 正規表現とは
+  正規表現を記述したファイルを読込み、正規表現で抽出したい部分を抽出し、新しいファイルを作成するツールです。
+  大量のログから必要な情報を抽出する時等に便利です。
+# 使用法
+  1, 正規表現を記述した'regexp.txt'と、正規表現で置換したいファイルを好きなディレクトリに置きます。
+  2, trim_fileを実行します; 'ruby trim_file.rb'
+  3, 先ほどファイルを置いたディレクトリのパスを ** 絶対パスで ** 入力します。 例) /path/to/directory
+  4, 'Finished!'のメッセージが出たら、置換されたファイルフォルダが同じディレクトリに作成されています
+# 注意点
+  - 正規表現は複数行書くことができます
+  - 正規表現ファイルや置換フォルダ名は'REGEXP_FILE', 'NEW_FOLDER'定数で変更できます
---
+++ trim_file.rb
@@ -0,0 +1,91 @@
+# FileMaker by Regexp
+#   Copyright Maki Kawahara
+#   Released under the MIT license
+# https://github.com/makicamel/file_maker
+
+require 'fileutils'
+
+class TrimFile
+
+  REGEXP_FILE = "regexp.txt"
+  NEW_FOLDER  = "new"
+
+  def main
+    p 'Input /path/to/folder and press return.'
+    p 'Notice: Only ** absolute ** path is available.'
+    folder_path = gets.chomp
+    return unless collect_path?(folder_path)
+
+    new_folder_path  = "#{folder_path}/#{NEW_FOLDER}"
+    regexp_file_path = "#{folder_path}/#{REGEXP_FILE}"
+    row_file_list    = Dir.glob("#{folder_path}/*").reject{|path| path == regexp_file_path || File.ftype(path) != 'file'}
+    regexp = get_regexp(regexp_file_path)
+    new_files = sub_files(row_file_list, regexp)
+
+    make_dir(new_folder_path)
+    make_new_files(new_files, new_folder_path)
+
+    p 'Finished!'
+  end
+
+  private
+  def collect_path?(folder_path)
+    unless File.exists?(folder_path)
+      p "Wrong path error. There is no file/directory for '#{folder_path}'"
+      return false
+    end
+    unless File.ftype(folder_path) == 'directory'
+      p "Wrong path error. '#{folder_path}' isn't a directory. "
+      return false
+    end
+    unless File.exists?("#{folder_path}/#{REGEXP_FILE}")
+      p('No file error. Place your regexp file in your inputted path.')
+      return false
+    end
+    if Dir.glob("#{folder_path}/*").size == 1
+      p "No file error. Place a file to be trimmed."
+      return false
+    end
+    true
+  end
+
+  def sub_files(row_file_list, regexp)
+    new_files = {}
+    row_file_list.each do |row_file_path|
+      File.open(row_file_path,'r') do |row_file|
+        lines = []
+        while line = row_file.gets
+          regexp.each {|exp| lines << $1 if line =~ exp }
+        end
+        new_files[File.basename(row_file_path).to_sym] = lines
+      end
+    end
+    new_files
+  end
+
+  def get_regexp(regexp_file_path)
+    regexp = []
+    File.open(regexp_file_path,'r') do |f|
+      while line = f.gets
+        regexp << Regexp.new(line.chomp)
+      end
+    end
+    regexp
+  end
+
+  def make_dir(new_folder_path)
+    FileUtils.mkdir(new_folder_path) unless File.exists?(new_folder_path)
+  end
+
+  def make_new_files(new_files, new_folder_path)
+    new_files.each do |new_file_name, new_lines|
+      File.open("#{new_folder_path}/#{Time.now.strftime('%Y%m%d%H%M%S')}_#{new_file_name}", 'w') do |f|
+        lines = new_lines.join("\n")
+        f.write(lines)
+      end
+    end
+  end
+end
+
+tf = TrimFile.new
+tf.main
